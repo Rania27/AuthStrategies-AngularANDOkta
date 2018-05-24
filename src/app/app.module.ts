e2a9bef7a78640fed1aedac84568c25877367796
Rania Bouker <rania.bouker@gmail.com>
2018-05-24 18:04:06 +0100

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { SearchService } from './shared';
+import { EditComponent } from './edit/edit.component';
+import { HomeComponent } from './home/home.component';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { OAuthService } from 'angular-oauth2-oidc';
+import {  AuthGuard,OktaAuthWrapper } from './shared';
 
+
+const appRoutes: Routes = [
+  {path: 'search', component: SearchComponent, canActivate: [AuthGuard]},
+  {path: 'edit/:id', component: EditComponent, canActivate: [AuthGuard]},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    EditComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    OAuthModule.forRoot(),
+    
+
   ],
-  providers: [],
+  providers: [SearchService,
+                AuthGuard,
+                OktaAuthWrapper],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/index.ts
@@ -0,0 +1,4 @@
+export * from './search/search.service';
+export * from './auth/auth.guard.service';
+export * from './auth/okta.auth.wrapper';
+// The reason for creating this file is so you can import multiple classes on a single line rather than having to import each individual class on separate lines
